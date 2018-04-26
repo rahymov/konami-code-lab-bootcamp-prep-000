58ae1363de2e768fb4047f8bb652bee6d7ddd223
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-26 14:10:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
   const ky = parseInt(e.detail) ;
   
