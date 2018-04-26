6d5a6b45f0e8de0f4dc32c98007f25bc145f9629
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-26 14:00:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(){
-    for(let i=0; i< code.length; ){
-      
+    for(let i=0; i< code.length; i++ ){
+      code[i] === 'keydown'
     }
   })
 }
